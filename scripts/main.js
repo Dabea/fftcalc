3d63e5a968518407a15464507338c906d5325aae
dabea <dabeaa@gmail.com>
2015-11-26 22:59:23 -0600

    added gender selector

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,37 @@
 /**
  * Created by aanderson on 11/22/2015.
  */
-$('#jobSelector').change(function(){
-    var value = $(this).val();
-    $('#job').class
-})
+
+jobsDataObject = {
+    1: "squire",
+    2: "chemist",
+    3: "knight",
+    4: "archer"
+
+};
+
+$(document).ready(function() {
+
+$('#jobChoices').change(function() {
+    changeJobImage();
+});
+
+$("input[name=gender]:radio").change(function () {
+    changeJobImage();
+});
+
+
+
+});
+
+function changeJobImage(){
+    var value = $('#jobChoices').val();
+    var currentJob =  $('#currentJob');
+    var gender = $('input[name=gender]:checked').val();
+    var fullJob = jobsDataObject[value] + "-" + gender;
+
+    currentJob.removeClass();
+    currentJob.addClass(fullJob);
+    console.log(jobsDataObject[value]);
+    console.log(fullJob);
+}
